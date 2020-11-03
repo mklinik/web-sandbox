ec8f10e15507815153000cf046ebfea495212ab0
Markus Klinik <mkl@lambdanaut.net>
2020-11-03 16:44:09 +0100

    add type annotations, stricter compiler options

--- ts-hello-world/app.ts
+++ ts-hello-world/app.ts
@@ -3,13 +3,16 @@
 // This is a line-by-line translation of the hello world web app from the node
 // documentation
 
-// const http = require('http');
+//const http = require('http');
 import http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+//const hostname = '127.0.0.1';
+const hostname: string = '127.0.0.1';
+//const port = 3000;
+const port: number = 3000;
 
-const server = http.createServer((req, res) => {
+//const server = http.createServer((req, res) => {
+const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
