15378cbb81fc082ac5968bac8256d72d7b23c754
Markus Klinik <mkl@lambdanaut.net>
2020-11-03 18:01:55 +0100

    output json based on request url

--- ts-hello-world/app.ts
+++ ts-hello-world/app.ts
@@ -11,11 +11,20 @@ const hostname: string = '127.0.0.1';
 //const port = 3000;
 const port: number = 3000;
 
+const elvis: { name: string, age: number } = { name: "elvis", age: 23 };
+const snuffles: { name: string, legs: number } = { name: "snuffles", legs: 4 };
+
 //const server = http.createServer((req, res) => {
 const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+  console.log(req.url);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Content-Type', 'text/json');
+  if( req.url == "/person" ) {
+    res.end(JSON.stringify(elvis));
+  }
+  else {
+    res.end(JSON.stringify(snuffles));
+  }
 });
 
 server.listen(port, hostname, () => {
