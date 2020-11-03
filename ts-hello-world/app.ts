2734bc90d43717ee1246512f15aa01ab39f75831
Markus Klinik <mkl@lambdanaut.net>
2020-11-03 18:05:37 +0100

    add named types

--- ts-hello-world/app.ts
+++ ts-hello-world/app.ts
@@ -11,8 +11,11 @@ const hostname: string = '127.0.0.1';
 //const port = 3000;
 const port: number = 3000;
 
-const elvis: { name: string, age: number } = { name: "elvis", age: 23 };
-const snuffles: { name: string, legs: number } = { name: "snuffles", legs: 4 };
+type Person = { name: string, age: number };
+type Pet = { name: string, legs: number };
+
+const elvis: Person = { name: "elvis", age: 23 };
+const snuffles: Pet = { name: "snuffles", legs: 4 };
 
 //const server = http.createServer((req, res) => {
 const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
