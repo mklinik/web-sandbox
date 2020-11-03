4ce438e8b6bbded8ed33b53996cfabb5fbe1ae52
Markus Klinik <mkl@lambdanaut.net>
2020-11-03 18:18:23 +0100

    comments

--- ts-hello-world/app.ts
+++ ts-hello-world/app.ts
@@ -1,14 +1,11 @@
 // https://www.typescriptlang.org/docs/handbook/migrating-from-javascript.html
 //
-// This is a line-by-line translation of the hello world web app from the node
-// documentation
+// This is an extension of the hello world web app from the node documentation
+// https://nodejs.org/en/docs/guides/getting-started-guide/
 
-//const http = require('http');
 import http = require('http');
 
-//const hostname = '127.0.0.1';
 const hostname: string = '127.0.0.1';
-//const port = 3000;
 const port: number = 3000;
 
 type Person = { name: string, age: number };
@@ -17,7 +14,6 @@ type Pet = { name: string, legs: number };
 const elvis: Person = { name: "elvis", age: 23 };
 const snuffles: Pet = { name: "snuffles", legs: 4 };
 
-//const server = http.createServer((req, res) => {
 const server: http.Server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
   console.log(req.url);
   res.statusCode = 200;
